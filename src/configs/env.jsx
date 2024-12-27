0197f07e5b464f69d599e8bac08c35d40166dd81
simal <>
2024-12-27 08:49:39 +0000

    added url

--- src/configs/env.jsx
+++ src/configs/env.jsx
@@ -1,3 +1,3 @@
-export const API_URL = "http://localhost:3000";
-
+// export const API_URL = "http://localhost:3000";
+export const API_URL = "http://44.223.14.188"
 // export const API_URL = "https://techno-ebon.vercel.app";
