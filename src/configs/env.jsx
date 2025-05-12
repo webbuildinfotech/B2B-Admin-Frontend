bd08c12deec061948ab90435b961061ce1e69912
zangula-dev <simalc@zangula.com>
2025-05-12 14:35:06 +0530

    added

--- src/configs/env.jsx
+++ src/configs/env.jsx
@@ -1,9 +1,3 @@
 // export const API_URL = "http://localhost:3000";
 
-// export const API_URL = "http://44.223.14.188"
-// export const API_URL = "https://api.rg-techno.com"
-// export const API_URL = "https://techno-ebon.vercel.app";
-
-// export const API_URL = "https://api.intecomart.com";
-
 export const API_URL = "https://api.intecomart.com"
